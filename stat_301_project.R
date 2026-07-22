4ae0dfb09ccebff34b029fdece7eeb85e532f713
Jayden-Yee <jaydenyee9@gmail.com>
2026-07-21 18:27:53 -0700

    yes yes

--- stat_301_project.R
+++ stat_301_project.R
@@ -1,9 +1,11 @@
 #Hidafsafdfdfddsahfjshdf
 library(usethis)
 library(gitcreds)
-use_git_remote(name = "origin", url = "https://github.com/leozjwang/Stat-301-Project.git", overwrite = TRUE)
 #idjakslfjkdsf
 create_github_token()
 gitcreds_set()
-use_git_remote(name = "origin",
-               url = "https://github.com/leozjwang/Stat-301-Project.git")
+use_git_remote(
+  name = "main", 
+  url = "https://github.com/leozjwang/Stat-301-Project.git",
+  overwrite = TRUE
+)
