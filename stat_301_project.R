6b8368ae2ea674ae45f14bf5e0673e7d765e1308
Jayden-Yee <jaydenyee9@gmail.com>
2026-07-21 18:13:04 -0700

    Hi i am jayden

--- stat_301_project.R
+++ stat_301_project.R
@@ -1,1 +1,9 @@
-#Hidafsaf
+#Hidafsafdfdfddsahfjshdf
+library(usethis)
+library(gitcreds)
+use_git_remote(name = "origin", url = "https://github.com/leozjwang/Stat-301-Project.git", overwrite = TRUE)
+#idjakslfjkdsf
+create_github_token()
+gitcreds_set()
+use_git_remote(name = "origin",
+               url = "https://github.com/leozjwang/Stat-301-Project.git")
