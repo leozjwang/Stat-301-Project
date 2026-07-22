ce4c0f704c4310fd655c1920861953a517d2cf3e
Cora <czeng888@students.cs.ubc.ca>
2026-07-21 18:36:57 -0700

    Libraries

--- main.r
+++ main.r
@@ -1,5 +1,10 @@
 library(readr)
+library(tidyverse)
+library(infer)
+library(broom)
 elec_data <- read.csv("complete_dataset.csv")
 print(summary(elec_data))
 
-#testtesttest
+
+
+# jwhakjdhakwjhdwa
