76165c55e62cd2d2d3556dfe1b8182ff85c8d81b
Jayden-Yee <jaydenyee9@gmail.com>
2026-07-21 18:30:53 -0700

    erasing all my data

--- stat_301_project.R
+++ stat_301_project.R
@@ -1,11 +1,1 @@
-#Hidafsafdfdfddsahfjshdf
-library(usethis)
-library(gitcreds)
-#idjakslfjkdsf
-create_github_token()
-gitcreds_set()
-use_git_remote(
-  name = "main", 
-  url = "https://github.com/leozjwang/Stat-301-Project.git",
-  overwrite = TRUE
-)
+
