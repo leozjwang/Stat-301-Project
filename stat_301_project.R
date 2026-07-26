076878bac4715959f164c86ab250d8a420523088
Jayden-Yee <jaydenyee9@gmail.com>
2026-07-25 19:31:21 -0700

    real commit for real real (the beginning)

--- stat_301_project.R
+++ stat_301_project.R
@@ -1,1 +1,13 @@
-# 
+library(dplyr)
+library(readr)
+elec_data <- read.csv("data/complete_dataset.csv")
+
+# Convert variables to correct type (date or factor)
+elec_data <- elec_data |>
+  mutate(date = as.Date(date),
+         school_day= as.factor(school_day),
+         holiday = as.factor(holiday))
+
+# Remove columns that we will most likely not use
+elec_filtered <- elec_data |>
+  select(-demand_pos_RRP, -demand_neg_RRP, -RRP_positive, -RRP_negative, -frac_at_neg_RRP)
